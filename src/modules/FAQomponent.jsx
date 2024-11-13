d5d4ecd531fb1d14e6d646e999dbd1f85232432e
Soumil Biswas Web <soumil.biswas19@hotmail.com>
2024-11-13 16:18:51 +0530

    Day 3: Add a feature. Completed

--- src/modules/FAQomponent.jsx
+++ src/modules/FAQomponent.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function FAQomponent({question="String"}) {
+
+    const [answer, setAnswer] = useState(false);
+
+    const showAnswer = () => setAnswer(!answer);
+
     return(
         <div className="faqomponent">
-            <p>{question}</p>
-            <p id="faqarrow">v</p>
+            <div className="flexpart">
+                <p>{question}</p>
+                <p id="faqarrow" onClick={showAnswer}>v</p>
+            </div>
+            <p className="answer" id={answer ? "show-answer" : "answer"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Audax negotium, dicerem impudens, nisi hoc institut translatum ad philosophos nostros esset.</p>
         </div>
     )
 }
